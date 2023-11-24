8fe53f0c31112fb13b82fd892e00fe742a95cc2c
xJustFox <dodydaversa@gmail.com>
2023-11-24 14:02:32 +0100

    [array_tails] Bonus completed

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,8 +1,12 @@
 let array = [];
 let array_num = parseInt(prompt('Quanti elementi vuoi in questo array?'));
+let array_element = parseInt(prompt('Quanti elementi vuoi vedere di questo array?'));
 
 for (let i = 1; i <= array_num; i++) {
     let num = Math.floor(Math.random()*100 + 1);
     array.push(num);
 }
 console.log(array);
+
+let element = array.slice(0,array_element);
+console.log(element);
